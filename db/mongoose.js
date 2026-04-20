7ed0108fe3cf92af7432816d0991932642f4a3a1
Osorio <dominioram@gmail.com>
2026-04-20 15:50:03 -0500

    last version

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,10 +1,16 @@
 const mongoose = require("mongoose");
 const { config } = require("../config/config");
+const safix = require("../models/safix.model"); // 👈 AGREGA ESTO
 
 const connect = async () => {
   try {
     await mongoose.connect(config.dbCnx);
     console.log("✅ Conectado a MongoDB");
+
+    // 🔥 LIMPIAR TOKENS VIEJOS (SOLO UNA VEZ)
+    await safix.deleteMany({});
+    console.log("🧹 safix limpiado");
+
   } catch (error) {
     console.error("❌ Error conectando a MongoDB:", error.message);
     process.exit(1);
